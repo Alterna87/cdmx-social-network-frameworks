dd6407be5fa6a52d63e9682fe81c9a28a01362b8
Your Name <you@example.com>
2018-09-20 14:08:34 -0500

    functional form signup component

--- src/components/SingInUp.js
+++ src/components/SingInUp.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
-
+import { firebaseApp } from '../firebase';
 
 class SingInUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      error: {
+        message: ''
+      }
+    }
+  }
+
+  signUp() {
+    console.log('this.state', this.state);
+    const { email, password } = this.state;
+    firebaseApp.auth().createUserWithEmailAndPassword(email, password)
+    .catch(error => { this.setState({ error }) })
+  }
 
   render() {
     return (
@@ -9,16 +26,20 @@ class SingInUp extends Component {
         <h3>Registrarse</h3>
         <div className="row">
           <div className="input-field col s6">
-          <input id="email" type="email" className="validate" />
-          <label for="email">Email</label>
+          <input id="email" type="email" className="validate" onChange = { event => this.setState({ email: event.target.value })}/>
+          <label>Email</label>
           </div>
           </div>
           <div className="row">
           <div className="input-field col s6">
-            <input id="password" type="password" className="validate" />
-            <label for="password">Password</label>
+            <input id="password" type="password" className="validate" onChange = { event => this.setState({ password: event.target.value })}/>
+            <label>Password</label>
             </div>
           </div>
+          <div className="row">
+          <a className="waves-effect waves-light btn" onClick = {() => this.signUp()} >Registrase</a>
+          </div>
+          <div>{ this.state.error.message }</div>
       </div>
     )
   }
