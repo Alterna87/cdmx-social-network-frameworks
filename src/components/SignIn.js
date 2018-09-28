f731dae673728f6353e7692a1950c0d9d91d13b8
Alterna87 <fvelueta@outlook.com>
2018-09-28 14:38:59 +0000

    add ghPages

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,4 +1,4 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import { firebaseApp } from '../firebase';
 import firebase from 'firebase';
 import logo  from '../assets/logo.png';
